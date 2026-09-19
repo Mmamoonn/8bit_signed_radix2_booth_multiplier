bd6a1b03915b22e6276e734d9a57abed418335f3
Mmamoonn <mmamoonmajid@gmail.com>
2026-09-20 01:46:52 +0500

    rtl: Add adder_subtractor.sv which implements 8-bit signed ALU

---
+++ rtl/adder_subtractor.sv
@@ -0,0 +1,17 @@
+module adder_subtractor (
+  input  logic signed [7:0] a,       // From Accumulator (P[15:8])
+  input  logic signed [7:0] m,       // From Multiplicand (M[7:0])
+  input  logic add_en, sub_en  	     // From Booth Encoder
+  output logic signed [7:0] result   // To 2-to-1 Multiplexer
+);
+    always_comb begin
+      //default_case: "If both add_en & sub_en signals are low (the 00 or 11 Booth condition), it defaults to outputting the unmodified Accumulator value a.
+      result = a;		 
+      if (add_en) begin
+        result = a + m;
+      end 
+      else if (sub_en) begin
+        result = a - m;
+      end
+    end
+endmodule
