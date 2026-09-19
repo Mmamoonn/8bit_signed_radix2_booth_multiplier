68367be319b5828f5807d81a5fdbd41916ccdd67
Mmamoonn <mmamoonmajid@gmail.com>
2026-09-20 02:27:12 +0500

    rtl: Implement controller.sv Moore FSM for booth multiplier control

---
+++ rtl/controller.sv
@@ -0,0 +1,62 @@
+module controller (
+    input  logic clk, rst_n, start, count_max,
+    output logic busy, done, load, arithmetic_shift_right, count_enable
+);
+  // State Encoding
+  typedef enum logic [1:0] {
+        IDLE      = 2'b00,
+        LOAD      = 2'b01,
+        CALCULATE = 2'b10,
+        DONE      = 2'b11
+    } state_t;
+
+    state_t current, next;
+
+    // Block 1: Synchronous State Register
+    always_ff @(posedge clk) begin
+      if (!rst_n) begin
+        current <= IDLE;
+      end 
+      else begin
+        current <= next;
+      end
+    end
+
+   // Block 2 + 3: Combinational Next-State Logic & Output Logic (Moore FSM)
+    always_comb begin
+      busy = 1'b0;
+      done = 1'b0;
+      load = 1'b0;
+      arithmetic_shift_right = 1'b0;
+      count_enable = 1'b0;
+      next = current;
+      
+      case (current)
+        IDLE: begin
+          if (start) begin
+            next = LOAD;
+          end
+        end
+        LOAD: begin
+          busy = 1'b1;
+          load = 1'b1;
+          next = CALCULATE; // Unconditional transition
+        end
+        CALCULATE: begin
+          busy = 1'b1;
+          arithmetic_shift_right = 1'b1;
+          count_enable = 1'b1;
+          if (count_max) begin
+            next = DONE;
+          end
+        end
+        DONE: begin
+          done = 1'b1;
+          next = IDLE;      // Unconditional transition
+        end
+        default: begin
+          next = IDLE;
+        end
+      endcase
+    end
+endmodule
